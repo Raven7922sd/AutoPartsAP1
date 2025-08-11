82f0480331fb4326baa89c17332d86690a99dbb1
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-08-11 01:45:16 -0600

    Ventas Dto creadas

---
+++ AutoPartsAP1/Components/Models/VentaDetalleDto.cs
@@ -0,0 +1,11 @@
+﻿namespace AutoPartsAP1.Components.Models;
+
+public class VentaDetalleDto
+{
+    public int ProductoId { get; set; }
+    public string ProductoNombre { get; set; }
+    public double Precio { get; set; }
+    public double Cantidad { get; set; }
+
+    public string Direccion { get; set; }
+}
---
+++ AutoPartsAP1/Components/Models/VentaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoPartsAP1.Components.Models;
+
+public class VentaDto
+{
+    public int VentaId { get; set; }
+    public DateTime Fecha { get; set; }
+    public Double Total { get; set; }
+
+    public List<VentaDetalleDto> Detalles { get; set; } = new();
+}
