8e3e2ddd6549e3f8e1092753e21cecde84243c5c
Raven7922sd <johan_reinoso@ucne.edu.do>
2025-08-11 01:45:03 -0600

    Factura Dto creadas

---
+++ AutoPartsAP1/Components/Models/FacturaDetalleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoPartsAP1.Components.Models;
+
+public class FacturaDetalleDto
+{
+    public string ProductoNombre { get; set; }
+    public string Categoria { get; set; }
+    public double PrecioUnitario { get; set; }
+    public double Cantidad { get; set; }
+    public double Subtotal { get; set; }
+}
---
+++ AutoPartsAP1/Components/Models/FacturaDto.cs
@@ -0,0 +1,10 @@
+﻿namespace AutoPartsAP1.Components.Models;
+
+public class FacturaDto
+{
+    public int VentaId { get; set; }
+    public DateTime Fecha { get; set; }
+    public string UsuarioEmail { get; set; }
+    public double Total { get; set; }
+    public List<FacturaDetalleDto> Detalles { get; set; } = new();
+}
